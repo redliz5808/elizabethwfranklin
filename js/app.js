0ace835fec098ad0835229676dfcad4593047dca
Elizabeth Franklin <elizabethwfranklin@gmail.com>
2025-10-22 15:37:08 -0400

    Build responsiveness into site

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 const body = document.getElementsByTagName("body")[0];
 const year = document.getElementById("year");
+const navLinks = document.getElementById("navLinks");
+const mobileNavToggler = document.getElementById("mobileNavToggler");
 
 if (year) {
     let todaysDate = new Date();
     let currentYear = todaysDate.getFullYear();
     year.innerText = `${currentYear} `;
 }
+
+mobileNavToggler.addEventListener("click", () => {
+    if (navLinks) {
+        !navLinks.classList.contains("open") ? navLinks.classList.add("open") : navLinks.classList.remove("open");
+    }
+});
